56aa99081a842a5af0f2ba6e31ab699dbc79ca05
a.parvaneh <amirparvaneh138@gmail.com>
2025-01-17 13:05:05 +0330

    add rabbitmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NModule } from './n/n.module';
 import { RabbitModule } from './rabbit/rabbit.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 
 @Module({
-  imports: [NModule, RabbitModule],
+  imports: [
+    RabbitModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/sales-invoice-app'),
+    RabbitmqModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RabbitmqService],
 })
 export class AppModule {}
